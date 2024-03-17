0835fa2dd52eec5c5a2a87f0a610ea84fae2e259
Darrin-Holtz <darrinholtz@gmail.com>
2024-03-16 21:04:45 -0400

    adding width & height

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -44,7 +44,11 @@ const Header = () => (
       transition={{ duration: 0.5, delayChildren: 0.5 }}
       className="app__header-img"
     >
-      <img src={images.profile} alt="profile_bg" />
+      <img src={images.profile} 
+      alt="profile_bg" 
+      width="100%"
+      height="auto"
+      />
       <motion.img
         whileInView={{ scale: [0, 1] }}
         transition={{ duration: 1, ease: 'easeInOut' }}
